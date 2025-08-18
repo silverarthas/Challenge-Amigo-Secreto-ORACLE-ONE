cac69147884bd30acb4c9e2e418f1eff6cb2fb68
silverarthas <rubendariocali@gmail.com>
2025-08-18 11:17:19 -0400

    Update app.js
    Codigo completo

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-/*Defino la variable de array para la lista de amigos a ingresar*/
-let listaAmigos = []; 
+let listaAmigos = [];
 /*funcion para capturar los nombres, almacenarlos en array y limpiar la caja*/
 function agregarAmigo() {
     let amigo = document.getElementById("amigo").value;
@@ -25,3 +24,25 @@ function updateList(){
         listaVisibleAmigos.innerHTML += `<li>${amigo}</li>`;    
     }
 }
+
+/*funcion para sortear amigos*/
+function sorteo(){
+    let seleccionado = "";
+    let amigo = listaAmigos.length;
+    let indice = Math.floor(Math.random()*listaAmigos.length);
+    if (amigo === 0) {
+        alert("Dioj! la lista no puede estar vacia!");
+        return
+    }
+    else{
+        seleccionado = listaAmigos[indice];
+        alert(`el amigo seleccionado es ${seleccionado}`)
+        let listaVisibleAmigos = document.getElementById("listaAmigos");
+        listaVisibleAmigos.innerHTML = `el amigo seleccionado es ${seleccionado}`;
+        listaAmigos=[];
+    }
+    console.log(`el numero aleatroio es ${indice}`);
+    console.log(`la cantidad de amigo(s) es/son ${amigo}`);
+}
+
+
